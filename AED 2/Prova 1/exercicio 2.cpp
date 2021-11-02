699df513600755053d8725220aa1e38294bd4c2e
André <87048023+andreflabr@users.noreply.github.com>
2021-11-02 20:14:41 -0300

    IF

---
+++ AED 2/Prova 1/exercicio 2.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+void ordena(int x[], int tamanho){
+    int i, j;
+    int aux;
+    
+    printf("Antes: ");
+    for(i=0; i<6; i++){
+        printf("%d ", x[i]);
+    }
+    printf("\n");
+
+    for (i=1; i<tamanho; i++){
+        aux=x[i];
+        j=i-1;
+        while((j>=0) && (aux<x[j])){
+            x[j+1] = x[j];
+            j--;
+        }
+        x[j+1] = aux;
+    }
+    printf("Depois: ");
+    for(i=0; i<6; i++){
+        printf("%d ", x[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int n[6] = {5, 4, 10, 1, 6, 2};
+
+    ordena(n, 6);
+return 0;
+}
