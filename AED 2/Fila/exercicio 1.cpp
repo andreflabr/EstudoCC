a3c8e1b1bb22e6aa236c13a6192a81caa18e36ce
André <87048023+andreflabr@users.noreply.github.com>
2021-11-28 17:54:17 -0300

    IF

---
+++ AED 2/Fila/exercicio 1.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+
+struct banco{
+    int identificador, posicao, idade, categoria;
+    char nome[40], deficiencia;
+};
+#define MAX 10
+struct banco v[MAX], temporario;
+int inicio=0, fim=0, i;
+
+int verificar(){
+    int i=fim-1;
+    if(v[i].categoria>temporario.categoria){
+        while(v[i].categoria>temporario.categoria && i>=inicio){
+            i--;
+        }
+    }else if(v[i].categoria<temporario.categoria){
+        i=fim-1;
+    }
+    return i+1;
+}
+
+void enqueue(){
+    int p;
+    temporario.posicao = fim;
+    printf("\nIdentificador: \n");
+    scanf("%d", &temporario.identificador);
+    printf("Posicao: \n");
+    scanf("%d", &temporario.posicao);
+    printf("Idade: \n");
+    scanf("%d", &temporario.idade);
+    printf("Nome: \n");
+    scanf(" %[^\n]", temporario.nome);
+    printf("deficiente: (s/n)\n");
+    scanf(" %[^\n]", temporario.deficiencia);
+
+    if(temporario.deficiencia == 's'){
+        if(temporario.idade >=60){
+            temporario.categoria=1;
+        }else{
+            temporario.categoria=2;
+        }
+    }
+    else{
+        if(temporario.idade>60){
+            temporario.categoria=3;
+        }else{
+            temporario.categoria=4;
+        }
+    }
+    if(fim==0){
+        v[fim].identificador = temporario.identificador;
+        v[fim].posicao = temporario.posicao;
+        strcpy(v[fim].nome, temporario.nome);
+        v[fim].idade = temporario.idade;
+        v[fim].deficiencia = temporario.deficiencia;
+        v[fim].categoria = temporario.categoria;
+        fim++;
+    }else{
+        p = verificar();
+        printf("%d\n", p);
+        for(i=fim; i>p; i--){
+            v[i].identificador = v[i-1].identificador;
+            v[i].posicao = v[i-1].posicao;
+            strcpy(v[i].nome, v[i-1].nome);
+            v[i].idade = v[i-1].idade;
+            v[i].deficiencia = v[i-1].deficiencia;
+            v[i].categoria = v[i-1].categoria;
+        }
+        v[p].identificador = temporario.identificador;
+        v[p].posicao = temporario.posicao;
+        v[p].idade = temporario.idade;
+        v[p].deficiencia = temporario.deficiencia;
+        v[p].categoria = temporario.categoria;
+        strcpy(v[p].nome, temporario.nome);
+        fim++;
+    }
+}
+
+int denqueue(){
+    int t;
+    if(fim!=0){
+        t=inicio;
+        inicio++;
+        return t;
+    }
+}
+
+void imprimir(void){
+    for(i=inicio; i<fim; i++){
+        printf("Nome: %s, idade: %d, deficiencia: %s\n", v[i].nome, v[i].idade, v[i].deficiencia );
+    }
+}
+
+int main(){
+    int x, y;
+
+    do{
+        printf("1 - Inserir | 2 - Remover | 3 - Imprimir | 4 - Primeiro da fila | 5 - Sair\n");
+        scanf("%d", &x);
+        switch(x){
+            case 1:
+                enqueue();
+                break;
+            case 2:
+                y = denqueue();
+                printf("Retirado identificador %d, de nome %s\n", v[y].identificador, v[y].nome);
+                break;
+            case 3:
+                imprimir();
+                break;
+            case 4:
+                printf("Nome: %s, idade: %d, deficiencia: %s\n", v[inicio].nome, v[inicio].idade, v[inicio].deficiencia );
+                break;
+        }
+    }while(x!=5);
+return 0;
+}
---
+++ AED 2/Fila/exercicio 2.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+int inicio = 0, fim = 0;
+#define MAX 5
+int fila[MAX];
+int *f;
+
+void enqueue(int valor) {
+    if (fim < MAX) {
+        f[fim] = valor;
+        printf("%d entrou na fila\n",f[fim]);
+        fim++;
+    } else {
+        printf("Fila cheia\n");
+    }
+}
+
+int denqueue(){
+    int r;
+
+    if (fim > 0) {
+        r=f[inicio];
+        inicio++;
+        return r;
+    } else {
+        printf("Fila vazia!\n");
+    }
+}
+
+void elementos(){
+    printf("Elementos na fila: %d\n", fim);
+}
+
+int main() {
+    int x, y, z;
+
+    f=(int *)malloc(MAX*sizeof(int));
+    do{
+        printf("1- Inserir\n2- Remover\n3- Imprimir primeiro da fila\n4- Imprimir ultimo da fila\n5- Mostrar quantos elementos tem na fila\n6- Sair\n");
+        scanf("%d", &x);
+        switch(x){
+            case 1:
+                printf("Digite o numero a ser inserido: \n");
+                scanf("%d", &z);
+                enqueue(z);
+                break;
+            case 2:
+                y=denqueue();
+                printf("O numero retirado foi: %d\n", y);
+                break;
+            case 3:
+                printf("Primeiro numero: %d\n", f[fim]);
+                break;
+            case 4:
+                printf("Ultimo numero: %d\n", f[inicio]);
+                break;
+            case 5:
+                elementos();
+                break;
+        }
+    }while(x!=6);
+
+    free(f);
+    return 0;
+}
+/*
+casos testes
+
+1
+6
+1
+5
+1
+4
+1
+3
+1
+2
+1
+1
+
+*/
