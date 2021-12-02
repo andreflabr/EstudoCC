6f1cae6b6ee0691a9a3ee8cd98d829a53af8002c
André <87048023+andreflabr@users.noreply.github.com>
2021-12-02 19:15:22 -0300

    IF

--- AED 2/Lista/exercicio 2.cpp
+++ AED 2/Lista/exercicio 2.cpp
@@ -6,21 +6,31 @@ typedef struct Nodo {
 };
 struct Nodo *pInicio=NULL;
 /////////////////////////////////////////////////////////////////////////////////
-void remover(struct Nodo* pNodo){
+void remover(int e){
+
+    bool flag=false;
+    struct Nodo *pAtual=pInicio;
+
     if(pInicio!=NULL){
-        if(pNodo==pInicio){
-            pInicio=pNodo->pProximo;
+        if(e==pInicio->numero){
+            pInicio=pAtual->pProximo;
+            free(pAtual);
         }else{
             struct Nodo *pAtual=pInicio;
             struct Nodo *pAnterior=NULL;
-            while(pAtual!=pNodo&&pAtual!=NULL){
-                pAnterior=pAtual;
-                pAtual=pAtual->pProximo;
+            while(pAtual != NULL && flag != true){
+                if(pAtual->numero != e){
+                    pAnterior = pAtual;
+                    pAtual=pAtual->pProximo;
+                }else {
+                    flag=true;
+                }
             }
             if(pAtual==NULL){
                 printf("O Elemento nao foi encontrado na lista!\n");
             }else{
                 pAnterior->pProximo=pAtual->pProximo;
+                free(pAtual);
             }
         }
     }
@@ -31,9 +41,9 @@ void remover(struct Nodo* pNodo){
 /////////////////////////////////////////////////////////////////////////////////
 void imprimir(){
     if(pInicio==NULL)
-        printf("A lista est� vazia!\n");
+        printf("A lista esta vazia!\n");
     else{
-        struct Nodo*pAtual=pInicio;
+        struct Nodo *pAtual=pInicio;
         while(pAtual!=NULL){
             printf("%d\n",pAtual->numero);
             pAtual=pAtual->pProximo;
@@ -41,25 +51,61 @@ void imprimir(){
     }
 }
 /////////////////////////////////////////////////////////////////////////////////
-void inserir(struct Nodo* pNodo){
-    if(pInicio==NULL)
-        pInicio=pNodo;
-    else{
-        struct Nodo* pFim=pInicio;
-        while(pFim->pProximo!=NULL){
-            pFim=pFim->pProximo;
+int inserir(struct Nodo* pNodo, int pos){
+    if(pInicio==NULL){
+        if(pos!=1){
+            return 0;
         }
-        pFim->pProximo=pNodo;
+        if(pNodo==NULL){
+            return 0;
+        }
+        pNodo->pProximo=NULL;
+        pInicio=pNodo;
+        return 1;
     }
-    pNodo->pProximo=NULL;
+    if(pos==1){
+        if(pNodo==NULL){
+            return 0;
+        }
+        pNodo->pProximo=pInicio;
+        pInicio=pNodo;
+        return 1;
+    }
+    struct Nodo *aux;
+    aux=pInicio;
+    int n=1;
+    while(n<pos-1 && aux!=NULL){
+        aux=aux->pProximo;
+        n++;
+    }
+    if(aux==NULL){
+        return 0;
+    }
+    if(pNodo==NULL){
+        return 0;
+    }
+    pNodo->pProximo=aux->pProximo;
+    aux->pProximo=pNodo;
+    return 1;
 }
 /////////////////////////////////////////////////////////////////////////////////
-//void inserirqqPosicao(struct Nodo* pNodo, struct Nodo* pNodoAnterior){
-
-//}
+int lista(){
+    int cont=0;
+    if(pInicio==NULL){
+        printf("A lista esta vazia!\n");
+    }else{
+        struct Nodo *pAtual=pInicio;
+        while(pAtual!=NULL){
+            pAtual=pAtual->pProximo;
+            cont++;
+        }
+    }
+    return cont;
+}
 /////////////////////////////////////////////////////////////////////////////////
 int main(){
-    int x, y;
+    struct Nodo *nod;
+    int x, p, c, elem, teste;
 
     do{
         printf("1-Inserir\n2-Remover\n3-Imprimir lista\n4-Mostrar quantos elementos tem na lista\n5-Sair\n");
@@ -67,24 +113,33 @@ int main(){
 
         switch(x){
         case 1:
-        
+            nod=(struct Nodo*)malloc(sizeof(struct Nodo));
             printf("Qual posicao voce deseja adicionar? \n");
-            scanf("%d", &y);
+            scanf("%d", &p);
 
             printf("Digite o numero a ser adicionado: \n");
-            //scanf("%d",);
+            scanf("%d", &nod->numero);
 
-            //inserirqqPosicao();
+            teste = inserir(nod,p);
+
+            if(teste=0){
+                printf("Posição invalida, teste novamente com outra posição!\n");
+            }else {
+                printf("Inserido!");
+            }
             break;
         case 2:
-            printf("Digite a posicao a ser removida");
-            //remover();
+            printf("Digite o elemento a ser removida");
+            scanf("%d", &elem);
+
+            remover(elem);
             break;
         case 3:
             imprimir();
             break;
         case 4:
-            printf("A lista possui %d elementos" );
+            c=lista();
+            printf("A lista possui %d elementos", c);
         }
     }while(x!=5);
 
