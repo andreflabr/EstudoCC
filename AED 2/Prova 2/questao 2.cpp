2e16fec2a723d30a3e34f7c175073471828b08b5
André <87048023+andreflabr@users.noreply.github.com>
2021-12-11 16:21:19 -0300

    IF

--- AED 2/Prova 2/questao 2.cpp
+++ AED 2/Prova 2/questao 2.cpp
@@ -1,85 +1,108 @@
-#include<bits/stdc++.h>
-
-typedef struct Nodo {
-	int numero;
-    struct Nodo *pProximo;
-};
-
-struct Nodo *pInicio=NULL;
-
-void inserir(struct Nodo* pNodo){
-    if(pInicio==NULL)
-        pInicio=pNodo;
-    else{
-        struct Nodo* pFim=pInicio;
-        while(pFim->pProximo!=NULL){
-            pFim=pFim->pProximo;
-        }
-        pFim->pProximo=pNodo;
-    }
-    pNodo->pProximo=NULL;
+#include <bits/stdc++.h>
+/////////////////////////////////////////////////////////////////////////////////////////////
+int fim=0;
+#define MAX 3
+int lista[MAX];
+/////////////////////////////////////////////////////////////////////////////////////////////
+int vazia(){
+	if(fim==0){
+		return 1;
+	}
+	return 0;
 }
-
-void remover(struct Nodo* pNodo){
-    if(pInicio!=NULL){
-        if(pNodo==pInicio){
-            pInicio=pNodo->pProximo;
-        }else{
-            struct Nodo *pAtual=pInicio;
-            struct Nodo *pAnterior=NULL;
-            while(pAtual!=pNodo&&pAtual!=NULL){
-                pAnterior=pAtual;
-                pAtual=pAtual->pProximo;
-            }
-            if(pAtual==NULL){
-                printf("O Elemento nao foi encontrado na lista!\n");
-            }else{
-                pAnterior->pProximo=pAtual->pProximo;
-            }
-        }
-    }
-    else{
-        printf("A lista esta vazia\n");
-    }
+/////////////////////////////////////////////////////////////////////////////////////////////
+void inserir(int valor){
+	if(fim==MAX){ 
+		printf("Nao inseriu %d, lista cheia\n", valor);
+	} else {
+		lista[fim]=valor;
+		fim++; 
+		printf("Inserido %d OK\n",valor);
+	}
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////////
+void remover(int pos){
+	int i;
+	if(!vazia()){
+		if(pos > fim-1){
+			printf("Posicao nao e valida (%d)\n",pos);
+		} else {
+			printf("Removido elemento %d da posicao (%d)\n",lista[pos],pos);
+			for(i= pos; i < fim; i++){
+				lista[i]=lista[i+1];
+			}
+			fim--;
+		}
+		} else {
+		printf("Lista vazia, nao removido\n");
+	}
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
 void imprimir(){
-    if(pInicio==NULL)
-        printf("A lista esta vazia!\n");
-    else{
-        struct Nodo*pAtual=pInicio;
-        while(pAtual!=NULL){
-            printf("%d\n",pAtual->numero);
-            pAtual=pAtual->pProximo;
+	int i;
+	if(!vazia()){ 
+		for(i=0; i<fim; i++){
+			printf("O elemento %d eh %d\n", i, lista[i]);
+		}
+	}else{
+		printf("Lista Vazia\n");
+	}
+}
+void procurar(int x){
+    int z=0;
+
+    for(int i=0; i<MAX; i++){
+        if(x == lista[i]){
+            printf("Valor encontrado!\n");  
+        }
+        else{
+            z++;
         }
     }
+    if(z==MAX){
+        printf("Valor nao encontrado!\n");
+    }
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////////
 int main(){
-    
-    struct Nodo nodo1;
-    nodo1.numero=10;
-    struct Nodo nodo2;
-    nodo2.numero=20;
-    struct Nodo nodo3;
-    nodo3.numero=5;
-    struct Nodo nodo4;
-    nodo4.numero=2;
-    //imprimir();//VAZIO
-    inserir(&nodo1);
-    inserir(&nodo2);
-    inserir(&nodo3);
-    printf("Depois das inclusoes:\n");
-    imprimir();//nodo1,nodo2,nodo3.
-    remover(&nodo1);
-    printf("Depois da exclusao:\n");
-    imprimir();//nodo2,nodo3.
-    inserir(&nodo4);
-    printf("Depois da inclusao do nodo4:\n");
-    imprimir();//nodo2,nodo3,nodo4.
-    remover(&nodo3);
-    printf("Depois da exclusao do nodo3:\n");
-    imprimir();//nodo2,nodo4
-    
-    return 0;
+    int x, valor, r_valor, procura;
+
+    do{
+        printf("1-inserir\n2-remover\n3-imprimir\n4-procurar\n5-sair\n");
+        scanf("%d", &x);
+
+        switch(x){
+            case 1:
+                printf("Digite o valor a ser inserido: \n");
+                scanf("%d", &valor);
+                inserir(valor);
+                break;
+            case 2:
+                printf("Digite a posicao a ser removida: \n");
+                scanf("%d", &r_valor);
+                remover(r_valor);
+                break;
+            case 3:
+                imprimir();
+                break;
+            case 4:
+                printf("Digite o valor a ser procurado: \n");
+                scanf("%d", &procura);
+                procurar(procura);
+        }
+    }while(x!=5);
+	return 0;
 }
+/////////////////////////////////////////////////////////////////////////////////////////////
+/*casos teste
+1
+19
+
+1
+34
+
+1
+23
+
+
+*/
