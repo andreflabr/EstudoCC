5e6c166bbc6943015b246083520cb0fd8102612c
André <87048023+andreflabr@users.noreply.github.com>
2023-03-25 22:23:13 -0300

    IF

--- AED 3/Arquivos/Exercicio_2/main.c
+++ AED 3/Arquivos/Exercicio_2/main.c
@@ -3,7 +3,7 @@
 #include<string.h>
 
 int main(){
-    char name_arquivo_1[30], name_arquivo_2[30], str[100], str_aux[100];
+    char name_arquivo_1[50], name_arquivo_2[50], str, str_aux;
     FILE *fp_1, *fp_2;
 
     printf("Digite o nome do arquivo: (sem .extensao)\n");
@@ -12,22 +12,18 @@ int main(){
     ///concatenando para nao precisar colocar a extensao do arquivo
     strcat(name_arquivo_1, ".txt");
 
-    if((fp_1=fopen(name_arquivo_1, "w"))==NULL){
-        printf("arquivo não pode ser aberto\n");
-        exit(1);
-    }
-    do{
-        printf("Entre com uma string (ENTER para sair) : \n");
-        gets(str);
-        strcat(str, "\n");
-        fputs(str, fp_1);
+    fp_1 = fopen(name_arquivo_1, "w+");  //criando arquivo 1
 
-///===================trocando arquivos========================
+    if(fp_1){
+        printf("\nDigite a string a adicionar!\n");
+        while(str != '\n' ){
+            scanf("%c", &str);
+            fputc(str, fp_1);
+        }
 
-        fgetc(fp_1);   //<----------------------------------------
-        printf("%s", str_aux);
+///=================== copiando conteudo para outro arquivo ========================
 
-
+        rewind(fp_1); //colocando o ponteiro no inicio do arquivo
 
         printf("\nDigite o nome do segundo arquivo: (sem .extensao)\n");
         gets(name_arquivo_2);
@@ -35,9 +31,18 @@ int main(){
         ///concatenando para nao precisar colocar a extensao do arquivo
         strcat(name_arquivo_2, ".txt");
 
+        fp_2 = fopen(name_arquivo_2, "w+"); //criando arquivo 2
 
-    } while(*str != '\n' );
+        while(!feof(fp_1)){
+            str_aux = fgetc(fp_1);
+            fputc(str_aux, fp_2);
+        }
+        printf("\nTexto copiado para o arquivo: %s", name_arquivo_2);
+        fclose(fp_1);
+        fclose(fp_2);
 
-
+    }
+    else {
+        printf("arquivo não pode ser aberto\n");
+    }
 }
-
