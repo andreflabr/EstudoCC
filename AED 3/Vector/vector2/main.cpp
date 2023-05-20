859608164a3720e10f1d560294d92aa87fc4e3f0
André <87048023+andreflabr@users.noreply.github.com>
2023-05-20 14:12:17 -0300

    IF

--- AED 3/Vector/vector2/main.cpp
+++ AED 3/Vector/vector2/main.cpp
@@ -1,9 +1,45 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+vector< pair< pair<double, string>, int > > aluno;
+int qtdd_alunos=2;
+
+int main(){
+    int id=0;
+    string nome;
+    double nota_1, nota_2, media;
+
+        for(id=1; id<=qtdd_alunos; id++){
+            cout << "Nome do aluno: " << endl;
+            cin >> nome;
+            cout << "Primeira nota: " << endl;
+            cin >> nota_1;
+            cout << "Segunda nota: " << endl;
+            cin >> nota_2;
+
+            media = (nota_1 + nota_2) /2;
+
+            aluno.push_back(make_pair(make_pair(-media, nome), id));
+        }
+    sort(aluno.begin(), aluno.end());
+
+    for(int id=0; id<qtdd_alunos; id++){
+        printf("%lf ", aluno[id].second);
+    }
+
+    printf("\n");
+
     return 0;
 }
+/*
+valores teste
+andre
+9.0
+10.0
+carlos
+7.5
+8.0
+
+*/
+
+
