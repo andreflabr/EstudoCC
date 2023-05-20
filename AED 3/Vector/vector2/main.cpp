b566c06ef15070ac5bb16c14315e198f8a523d57
André <87048023+andreflabr@users.noreply.github.com>
2023-05-20 14:43:07 -0300

    IF

--- AED 3/Vector/vector2/main.cpp
+++ AED 3/Vector/vector2/main.cpp
@@ -1,32 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector< pair< pair<double, string>, int > > aluno;
-int qtdd_alunos=2;
+    vector< pair< pair<double, string>, int > > aluno;
+    int qtdd_alunos=2;
 
 int main(){
-    int id=0;
     string nome;
+    int  id=0;
     double nota_1, nota_2, media;
 
-        for(id=1; id<=qtdd_alunos; id++){
-            cout << "Nome do aluno: " << endl;
-            cin >> nome;
-            cout << "Primeira nota: " << endl;
-            cin >> nota_1;
-            cout << "Segunda nota: " << endl;
-            cin >> nota_2;
+    for(id=1; id<=qtdd_alunos; id++){
+        cout << "Nome do aluno: " << endl;
+        cin >> nome;
+        cout << "Primeira nota: " << endl;
+        cin >> nota_1;
+        cout << "Segunda nota: " << endl;
+        cin >> nota_2;
 
-            media = (nota_1 + nota_2) /2;
+        media = (nota_1 + nota_2) / 2;
 
-            aluno.push_back(make_pair(make_pair(-media, nome), id));
-        }
+        aluno.push_back(make_pair(make_pair(-media, nome), id));
+    }
     sort(aluno.begin(), aluno.end());
 
     for(int id=0; id<qtdd_alunos; id++){
-        printf("%lf ", aluno[id].second);
+        printf("%d ", aluno[id].second);
     }
-
     printf("\n");
 
     return 0;
